2626f7764a28208cf9ed69bb686ee9d33ddfe1f2
Ray Lothian <ray.lothian@yahoo.com>
2022-05-24 03:29:30 -0400

    moving to GT

---
+++ data/view/index.js
@@ -0,0 +1,82 @@
+chrome.storage.local.get({
+  size: 50,
+  width: 1
+}, async prefs => {
+  document.body.style.setProperty('--size', prefs.size + 'px');
+  document.body.style.setProperty('--thickness', prefs.width + 'px');
+
+  document.getElementById('unit').dataset.msg = prefs.size + 'px';
+});
+chrome.storage.onChanged.addListener(ps => {
+  if (ps.size) {
+    document.body.style.setProperty('--size', ps.size.newValue + 'px');
+    document.getElementById('unit').dataset.msg = ps.size.newValue + 'px';
+  }
+  if (ps.width) {
+    document.body.style.setProperty('--thickness', ps.width.newValue + 'px');
+  }
+});
+
+const pos = {
+  pageX: 0,
+  pageY: 0
+};
+
+const ruler = document.getElementById('ruler');
+const mousemove = e => {
+  const x1 = Math.min(e.pageX, pos.pageX);
+  const y1 = Math.min(e.pageY, pos.pageY);
+  const x2 = Math.max(e.pageX, pos.pageX);
+  const y2 = Math.max(e.pageY, pos.pageY);
+
+  ruler.style.left = x1 + 'px';
+  ruler.style.top = y1 + 'px';
+  ruler.style.width = (x2 - x1) + 'px';
+  ruler.style.height = (y2 - y1) + 'px';
+  if (x2 - x1 > 100 || y2 - y1 > 100) {
+    ruler.dataset.start = `${x1}px × ${y1}px`;
+    ruler.dataset.end = `${x2}px × ${y2}px`;
+  }
+  else {
+    ruler.dataset.start = '';
+    ruler.dataset.end = '';
+  }
+  if (x2 - x1 > 100 && y2 - y1 > 100) {
+    ruler.textContent = `${x2 - x1}px × ${y2 - y1}px`;
+  }
+  else {
+    ruler.textContent = '';
+  }
+};
+
+window.addEventListener('mousedown', e => {
+  pos.pageX = e.pageX;
+  pos.pageY = e.pageY;
+  ruler.style.width = 0;
+  ruler.style.height = 0;
+  ruler.textContent = '';
+  ruler.dataset.start = '';
+  ruler.dataset.end = '';
+
+  document.body.dataset.busy = true;
+
+  window.addEventListener('mousemove', mousemove);
+  window.addEventListener('mouseup', () => {
+    window.removeEventListener('mousemove', mousemove);
+  }, {
+    once: true
+  });
+});
+
+window.addEventListener('keydown', e => {
+  if (e.code === 'Escape') {
+    if (window.top === window) {
+      window.close();
+    }
+    else {
+      chrome.runtime.sendMessage({
+        method: 'remove'
+      });
+    }
+  }
+});
---
+++ worker.js
@@ -0,0 +1,214 @@
+chrome.runtime.onMessage.addListener((request, sender) => {
+  chrome.action.setIcon({
+    tabId: sender.tab.id,
+    path: {
+      '16': 'data/icons/16.png',
+      '32': 'data/icons/32.png',
+      '48': 'data/icons/48.png'
+    }
+  });
+  chrome.scripting.executeScript({
+    target: {
+      tabId: sender.tab.id
+    },
+    func: () => {
+      [...document.querySelectorAll('.pgrwx')].forEach(e => e.remove());
+    }
+  });
+});
+
+chrome.action.onClicked.addListener(async tab => {
+  try {
+    const r = await chrome.scripting.executeScript({
+      target: {
+        tabId: tab.id
+      },
+      func: tabId => {
+        const es = [...document.querySelectorAll('.pgrwx')];
+
+        if (es.length) {
+          es.forEach(e => e.remove());
+
+          return true;
+        }
+        else {
+          const f = document.createElement('iframe');
+          f.style = `
+            position: fixed;
+            z-index: 2147483647;
+            border: none;
+            left: 0;
+            top: 0;
+            width: 100vw;
+            height: 100vh;
+            left: 0;
+            top: 0
+          `;
+          f.classList.add('pgrwx');
+          f.src = chrome.runtime.getURL('/data/view/index.html?tabId=' + tabId);
+          document.body.appendChild(f);
+          f.focus();
+
+          return false;
+        }
+      },
+      args: [tab.id]
+    });
+    chrome.action.setIcon({
+      tabId: tab.id,
+      path: {
+        '16': 'data/icons/' + (r[0].result ? '' : 'active/') + '16.png',
+        '32': 'data/icons/' + (r[0].result ? '' : 'active/') + '32.png',
+        '48': 'data/icons/' + (r[0].result ? '' : 'active/') + '48.png'
+      }
+    });
+  }
+  catch (e) {
+    chrome.tabs.create({
+      url: 'data/view/index.html',
+      index: tab.index + 1
+    });
+  }
+});
+
+const context = () => chrome.storage.local.get({
+  size: 50,
+  width: 1
+}, prefs => {
+  console.log(prefs);
+  chrome.contextMenus.create({
+    title: 'Grid Size',
+    id: 'size',
+    contexts: ['action']
+  });
+  chrome.contextMenus.create({
+    title: '50px',
+    id: 'size.50',
+    parentId: 'size',
+    contexts: ['action'],
+    type: 'radio',
+    checked: prefs.size === 50
+  });
+  chrome.contextMenus.create({
+    title: '100px',
+    id: 'size.100',
+    parentId: 'size',
+    contexts: ['action'],
+    type: 'radio',
+    checked: prefs.size === 100
+  });
+  chrome.contextMenus.create({
+    title: '150px',
+    id: 'size.150',
+    parentId: 'size',
+    contexts: ['action'],
+    type: 'radio',
+    checked: prefs.size === 150
+  });
+  chrome.contextMenus.create({
+    title: '200px',
+    id: 'size.200',
+    parentId: 'size',
+    contexts: ['action'],
+    type: 'radio',
+    checked: prefs.size === 200
+  });
+  chrome.contextMenus.create({
+    title: '250px',
+    id: 'size.250',
+    parentId: 'size',
+    contexts: ['action'],
+    type: 'radio',
+    checked: prefs.size === 250
+  });
+
+  chrome.contextMenus.create({
+    title: 'Border Thinness',
+    id: 'width',
+    contexts: ['action']
+  });
+  chrome.contextMenus.create({
+    title: '1px',
+    id: 'width.1',
+    parentId: 'width',
+    contexts: ['action'],
+    type: 'radio',
+    checked: prefs.width === 1
+  });
+  chrome.contextMenus.create({
+    title: '2px',
+    id: 'width.2',
+    parentId: 'width',
+    contexts: ['action'],
+    type: 'radio',
+    checked: prefs.width === 2
+  });
+  chrome.contextMenus.create({
+    title: '3px',
+    id: 'width.3',
+    parentId: 'width',
+    contexts: ['action'],
+    type: 'radio',
+    checked: prefs.width === 3
+  });
+  chrome.contextMenus.create({
+    title: '4px',
+    id: 'width.4',
+    parentId: 'width',
+    contexts: ['action'],
+    type: 'radio',
+    checked: prefs.width === 4
+  });
+  chrome.contextMenus.create({
+    title: '5px',
+    id: 'width.5',
+    parentId: 'width',
+    contexts: ['action'],
+    type: 'radio',
+    checked: prefs.width === 5
+  });
+});
+chrome.runtime.onStartup.addListener(context);
+chrome.runtime.onInstalled.addListener(context);
+
+chrome.contextMenus.onClicked.addListener(info => {
+  console.log(info);
+  if (info.menuItemId.startsWith('width.')) {
+    chrome.storage.local.set({
+      'width': Number(info.menuItemId.slice(6))
+    });
+  }
+  else if (info.menuItemId.startsWith('size.')) {
+    chrome.storage.local.set({
+      'size': Number(info.menuItemId.slice(5))
+    });
+  }
+});
+
+/* FAQs & Feedback */
+{
+  const {management, runtime: {onInstalled, setUninstallURL, getManifest}, storage, tabs} = chrome;
+  if (navigator.webdriver !== true) {
+    const page = getManifest().homepage_url;
+    const {name, version} = getManifest();
+    onInstalled.addListener(({reason, previousVersion}) => {
+      management.getSelf(({installType}) => installType === 'normal' && storage.local.get({
+        'faqs': true,
+        'last-update': 0
+      }, prefs => {
+        if (reason === 'install' || (prefs.faqs && reason === 'update')) {
+          const doUpdate = (Date.now() - prefs['last-update']) / 1000 / 60 / 60 / 24 > 45;
+          if (doUpdate && previousVersion !== version) {
+            tabs.query({active: true, currentWindow: true}, tbs => tabs.create({
+              url: page + '?version=' + version + (previousVersion ? '&p=' + previousVersion : '') + '&type=' + reason,
+              active: reason === 'install',
+              ...(tbs && tbs.length && {index: tbs[0].index + 1})
+            }));
+            storage.local.set({'last-update': Date.now()});
+          }
+        }
+      }));
+    });
+    setUninstallURL(page + '?rd=feedback&name=' + encodeURIComponent(name) + '&version=' + version);
+  }
+}
